3e88330cfd7a75334c81d096de9b75cc030d0e9d
Hamed Darragi <hd@xanweb.com>
2021-10-07 10:41:42 +0100

    Remove default asset localization classes, should be replaced by frontend/backend ones

--- Config/JavascriptAssetDefaults.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Xanweb\C5\JsLocalization\Config;
-
-use Xanweb\C5\JsLocalization\AssetLocalizationCollection;
-
-class JavascriptAssetDefaults extends AssetLocalizationCollection
-{
-}
